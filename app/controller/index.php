d4ab54dbb4182d436e231871dd2ca9bcc8ad1075
PHPerJiang <554246432@qq.com>
2018-07-03 22:05:41 +0800

    添加了数据库Db类，可以进行model层的操作

--- app/controller/index.php
+++ app/controller/index.php
@@ -3,6 +3,10 @@ namespace app\controller;
 
 class index{
     public function index(){
-        debug('success');
+        debug('欢迎使用PHPer的简易框架','',false);
+//         $pdo=\core\lib\Db::getInstance();
+//         $stmt=$pdo->query('select * from user');
+//         $row=$stmt->fetchAll(\PDO::FETCH_ASSOC);
+//         debug($row);
     }
 }
